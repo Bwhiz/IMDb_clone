ad4e76318a8b6ae37f2c8a166af2d442ffb0869f
Bwhiz <ejelonubenedict99@gmail.com>
2022-07-29 13:38:26 +0100

    api view for GET request added

--- bwhiz_movie/watchlist_app/api/serializers.py
+++ bwhiz_movie/watchlist_app/api/serializers.py
@@ -3,5 +3,5 @@ from rest_framework import serializers
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
-    descr = serializers.CharField()
+    description = serializers.CharField()
     active = serializers.BooleanField()
--- bwhiz_movie/watchlist_app/api/views.py
+++ bwhiz_movie/watchlist_app/api/views.py
@@ -1,15 +1,16 @@
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
-
+@api_view()
 def movie_list(request):
     movie = Movie.objects.all()
-    serializer = MovieSerializer(movie)
+    serializer = MovieSerializer(movie, many=True)
     
     return Response(serializer.data)
 
-
+@api_view()
 def movie_detail(request, pk):
     movie = Movie.objects.get(pk=pk)
     serializer = MovieSerializer(movie)
