bd07bbdde33215c1f1238635b964589ca3e3f1ce
Bwhiz <ejelonubenedict99@gmail.com>
2022-08-11 17:30:38 +0100

    Created user and option to submit reviews by user as foreign key

--- bwhiz_movie/watchlist_app/api/views.py
+++ bwhiz_movie/watchlist_app/api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 # from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from django.http import JsonResponse
 from rest_framework import generics , viewsets #, mixins
 
@@ -62,11 +63,21 @@ def homepage_(request):
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
     
+    def get_queryset(self):
+        return Review.objects.all()
+    
+    
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         movie = WatchList.objects.get(pk=pk)
         
-        serializer.save(watchlist=movie)
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=movie, review_user=review_user)
+        
+        if review_queryset.exists():
+            raise ValidationError(" you have already sent out a review for this movie")
+        
+        serializer.save(watchlist=movie, review_user=review_user)
 
 class ReviewList(generics.ListAPIView):
     # queryset = Review.objects.all()
